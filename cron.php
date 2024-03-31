3f1ac41e03f10489532096e56186f3e64606a710
Jan Eike Suchard <jan@suchard.cloud>
2024-03-31 09:44:05 +0200

    updating cron job

--- cron.php
+++ cron.php
@@ -1,10 +1,13 @@
 <?php
 
 
-function cron() {
-	$movies = get_movies();
+require_once 'common/get-movies.php';
+
+function map_cron() {
+	$movies = map_get_movies();
 	foreach ( $movies as $movie ) {
 		$movie->postToDiscord();
 		$movie->postToMastodon();
 	}
+	update_option( OptionGroup . '-last-cron', date( 'd.m.Y H:i:s' ) );
 }
