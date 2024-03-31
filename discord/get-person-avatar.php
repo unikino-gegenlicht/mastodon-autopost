7fea3e291bd2c7a5bb325853e979e402ec2ee27c
Jan Eike Suchard <jan@suchard.cloud>
2024-03-31 23:04:58 +0200

    [fix] using wrong field of post for name resolution

--- discord/get-person-avatar.php
+++ discord/get-person-avatar.php
@@ -13,7 +13,7 @@ function map_get_person_avatar_url( MAP_Movie $movie ): string {
 	$posts = new WP_Query( $filter );
 
 	while ($posts->have_posts()) {
-		if ($posts->post->post_name == $personName) {
+		if ($posts->post->post_title == $personName) {
 			return get_the_post_thumbnail_url($posts->post);
 
 		}
