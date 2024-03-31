d2871f7a7b7124ee957e45fe099107c82af5ffdf
Jan Eike Suchard <jan@suchard.cloud>
2024-03-31 09:44:29 +0200

    adding overview html

---
+++ views/index.php
@@ -0,0 +1,37 @@
+<?php
+
+function map_overview_html() {
+	?>
+    <div class="wrap">
+        <h1><?= esc_html( get_admin_page_title() ) ?></h1>
+        <br/>
+        <br/>
+        <table>
+            <tr>
+                <th>Letzte Cron-Ausführung</th>
+                <td><?= get_option(OptionGroup.'-last-cron', 'unbekannt')?></td>
+            </tr>
+        </table>
+
+        <h2>Tests</h2>
+        <p>
+            In diesem Abschnitt können die beiden Portale getestet werden. Hierfür wird ein standardisierter Post auf
+            den jeweiligen Portalen veröffentlicht.
+        </p>
+        <script>
+            function map_test_discord() {
+                jQuery.post(ajaxurl, {action: "movie_autopost_test_discord"})
+            }
+            function map_test_mastodon() {
+                jQuery.post(ajaxurl, {action: "movie_autopost_test_mastodon"})
+            }
+        </script>
+        <button class="button" onclick="map_test_discord()">
+            Test Discord Autopost
+        </button>
+        <button class="button" onclick="map_test_mastodon()">
+            Test Mastodon Autopost
+        </button>
+    </div>
+	<?php
+}
